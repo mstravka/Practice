aa9bf54c498d7276ac51e4b4d41f50944357fec8
mstravka <zenadenisenko900@gmail.com>
2026-02-04 11:50:19 +0200

    Create 1.5.py

---
+++ 1.5.py
@@ -0,0 +1,15 @@
+word = "Programming"
+print(word[0])
+print(word[2:7])
+print(len(word))
+print(word.lower())
+
+nums = [10, 20, 30, 40, 50]
+print(nums[0])
+print(nums[1:4])
+
+for letter in word:
+    print(letter)
+
+for n in nums:
+    print(n)
